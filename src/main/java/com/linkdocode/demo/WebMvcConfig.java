6b6d507e5dcc4880a6d45d934c485758c39edcbf
Leenawat Papahom <leenawat.pa@gmail.com>
2017-10-21 09:07:21 +0700

    second commit

---
+++ src/main/java/com/linkdocode/demo/WebMvcConfig.java
@@ -0,0 +1,25 @@
+package com.linkdocode.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		// resolver.setPrefix("/");
+		// resolver.setSuffix(".jsp");
+		// resolver.setViewClass(JstlView.class);
+		registry.viewResolver(resolver);
+	}
+}
